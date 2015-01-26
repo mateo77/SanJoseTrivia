1523120d10c0dd1ec25ce624d85e8d2fc168e354
mateo77 <mateo.gutierrez.7@gmail.com>
2015-01-26 15:55:02 -0200

    Entidades
    asd

---
+++ Proyecto Final Segundo Año/Persistencia/PersistenciaAdministrador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using EntidadesCompartidas;
+
+namespace Persistencia
+{
+    internal class PersistenciaAdministrador : IPersistenciaAdministrador
+    {
+        //Singleton
+        private static PersistenciaAdministrador _instancia = null; 
+
+        private PersistenciaAdministrador() {} 
+
+        public static PersistenciaAdministrador GetInstancia()  
+        {
+            if (_instancia == null) 
+                _instancia = new PersistenciaAdministrador(); 
+            return _instancia; 
+        }
+
+
+    }
+}
