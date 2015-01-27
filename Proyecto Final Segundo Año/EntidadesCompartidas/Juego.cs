57160e6a821749bbac5676b7438575977b0800d1
mateo77 <mateo.gutierrez.7@gmail.com>
2015-01-27 11:10:16 -0200

    Proyecto Final
    vpe

---
+++ Proyecto Final Segundo Año/EntidadesCompartidas/Juego.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntidadesCompartidas
+{
+    [Serializable]
+
+    public class Juego
+    {
+        //atributos
+        private int _idJuego;
+        private int _tiradas;
+        private DateTime _fechaInicio;
+        private DateTime _fechaFin;
+        private Jugador _jugador;
+        private List<Pregunta> _preguntas;
+
+
+        //propiedades
+        public int Id 
+        {
+            get { return _idJuego ; }
+            set { _idJuego = value; }
+        }
+
+        public int Tiradas
+        {
+            get { return _tiradas ; }
+            set { _tiradas = value; }
+        }
+
+        public DateTime FechaInicio
+        {
+            get { return _fechaInicio; }
+            set { _fechaInicio = value; }
+        }
+
+        public DateTime FechaFin
+        {
+            get { return _fechaFin; }
+            set { _fechaFin = value; }
+        }
+
+        public Jugador Jugador
+        {
+            get { return _jugador; }
+            set { _jugador = value; }
+        }
+
+        public List<Pregunta> Preguntas
+        {
+            get { return _preguntas; }
+            set { _preguntas = value; }
+        }
+
+        //constructor
+        public Juego(int oIdJuego, int oTiradas, DateTime oFechaInicio, DateTime oFechaFin, Jugador oJugador, List<Pregunta> oPreguntas)
+        {
+            Id = oIdJuego;
+            Tiradas = oTiradas;
+            FechaInicio = oFechaInicio;
+            FechaFin = oFechaFin;
+            Jugador = oJugador;
+            Preguntas = oPreguntas;
+        }
+
+        public Juego()
+        {
+        }
+           
+
+    }
+}
---
+++ Proyecto Final Segundo Año/EntidadesCompartidas/Pregunta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntidadesCompartidas
+{
+    [Serializable]
+
+    public class Pregunta
+    {
+        //atributos
+        private int _idPregunta;
+        private string _textoPregunta;
+        private string _tipo;
+        private List<Respuesta> _respuestas;
+
+        //atributos
+        public int IdPregunta  
+        {
+            get { return _idPregunta; }
+            set { _idPregunta = value; }
+        }
+
+        public string TextoPregunta
+        {
+            get { return _textoPregunta ; }
+            set { _textoPregunta = value; }
+        }
+
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+        public List<Respuesta> Respuestas
+        {
+            get { return _respuestas; }
+            set { _respuestas = value; }
+        }
+
+        //constructor
+        public Pregunta(int oPregunta, string oTextoPregunta, string oTipo, List<Respuesta> oRespuestas)
+        {
+            IdPregunta = oPregunta;
+            TextoPregunta = oTextoPregunta;
+            Tipo = oTipo;
+            Respuestas = oRespuestas;
+        }
+
+        public Pregunta()
+        {
+        }
+
+    }
+}
